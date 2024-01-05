36765cf294eb775e24601005c884be648bde22ac
Phi-Laptop <nguyenhongphi2807@gmail.com>
2024-01-05 17:35:29 +0700

    working on 02_prepare_base_model.ipynb

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -37,3 +37,6 @@ class DataIngestion:
         os.makedirs(unzip_path, exist_ok=True)
         with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
             zip_ref.extractall(unzip_path)
+
+if __name__ == "__main__":
+    print("Libraries imported successfully.")

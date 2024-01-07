a3769ba0ae70ace192c7bd7caa0d7041429bb581
Phi Nguyen <nguyenhongphi2807@gmail.com>
2024-01-08 03:02:26 +0700

    modular training part added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelPipeline
+from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 
 STAGE_NAME = 'Data Ingestion Stage'
@@ -19,6 +20,16 @@ try:
         logger.info(f">>> Stage {STAGE_NAME} started <<<")
         prepare_base_model_pipeline = PrepareBaseModelPipeline()
         prepare_base_model_pipeline.main()
+        logger.info(f">>> Stage {STAGE_NAME} completed <<<")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+try:
+        logger.info(f">>> Stage {STAGE_NAME} started <<<")
+        model_training_pipeline = ModelTrainingPipeline()
+        model_training_pipeline.main()
         logger.info(f">>> Stage {STAGE_NAME} completed <<<")
 except Exception as e:
         logger.exception(e)
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -8,6 +8,7 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
 
+
 @dataclass(frozen=True)
 class PrepareBaseModelConfig:
     root_dir: Path
@@ -18,3 +19,16 @@ class PrepareBaseModelConfig:
     params_include_top: bool
     params_weights: str
     params_classes: int
+
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    '''Entity that contains information needed for training'''
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
